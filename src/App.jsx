6b55f743f36be7b8e063b951a52d479de6af3aaf
miqueiasbelfort <kawasousa1554@gmail.com>
2022-04-29 09:20:14 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+
+        <div className="container">
+          <Routes>
+
+            <Route path='/' element={<Home />}/>
+            <Route path='/about' element={<About />}/>
+
+          </Routes>
+        </div>
+
+      </BrowserRouter>
     </div>
   )
 }
