ae4a2e4c69e1ea6c83930b01de293210b8f46821
miqueiasbelfort <kawasousa1554@gmail.com>
2022-04-29 09:06:02 -0300

    firebase

---
+++ firebase/config.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLKScySsJ4OxdNzlJMjk42NgeiVHRPoWo",
+  authDomain: "miniblog-594a8.firebaseapp.com",
+  projectId: "miniblog-594a8",
+  storageBucket: "miniblog-594a8.appspot.com",
+  messagingSenderId: "363651887867",
+  appId: "1:363651887867:web:b4708c8fad604ef612365e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export { db }
