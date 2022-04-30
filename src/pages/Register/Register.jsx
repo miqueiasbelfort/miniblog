967742da4c8c95add2e43d1e95aa64b14f378a83
miqueiasbelfort <kawasousa1554@gmail.com>
2022-04-30 10:27:58 -0300

    register firebase

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,7 @@
 import styles from "./Register.module.css"
 
 import {useState, useEffect} from "react"
+import { useAuthentication } from "../../hooks/useAuthentication"
 
 const Register = () => {
   
@@ -10,7 +11,9 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState("")
   const [error, setError] = useState("")
 
-  const handleSubmit = (e) => {
+  const {createUser, error: authError, loading} = useAuthentication()
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
     setError("")
@@ -26,8 +29,18 @@ const Register = () => {
       return
     }
 
-    console.log(user)
+    const res = await createUser(user)
+
+    console.log(res)
   }
+
+  useEffect(() => {
+
+    if(authError){
+      setError(authError)
+    }
+
+  }, [authError])
 
   return (
     <div className={styles.register}>
@@ -83,7 +96,8 @@ const Register = () => {
             />
         </label>
 
-        <button className="btn">Cadastrar</button>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && <button className="btn" disabled>Aguarde...</button>}
         {error && <p className="error">{error}</p>}
       </form>
     </div>
