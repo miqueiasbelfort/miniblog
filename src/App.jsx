19dc500c1bb0d2c9eed749aac346f498babfa42a
miqueiasbelfort <kawasousa1554@gmail.com>
2022-05-03 10:00:49 -0300

    edit post

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import CreatePost from './pages/CreatePost/CreatePost'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Search from './pages/Search/Search'
 import Post from './pages/Post/Post'
+import EditPost from './pages/EditPost/EditPost'
 
 
 
@@ -60,6 +61,7 @@ function App() {
             <Route path='/register' element={!user ? <Register />: <Navigate to="/"/>}/>
             <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login"/>}/>
             <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login"/>}/> 
+            <Route path='/posts/edit/:id' element={user ? <EditPost /> : <Navigate to="/login"/>}/> 
           </Routes>
         </div>
 
